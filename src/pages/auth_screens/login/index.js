c95cf41b3c9cb1b19a92019d1a7fd9da4b052aa7
Sumit-Bhardwaj <sumit.bhardwaj.eSparkbiz@gmail.com>
2023-05-15 14:01:32 +0530

    15-05-23 added yup validation in all forms

--- src/pages/auth_screens/login/index.js
+++ src/pages/auth_screens/login/index.js
@@ -1,210 +1,151 @@
+import * as yup from "yup";
+
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useForm } from "react-hook-form";
 import { Link, useNavigate } from "react-router-dom";
-import "../../../assets/styles/login.css";
-import { useEffect, useState } from "react";
-import { FormField } from "../../../components/FormFields/FormField";
 
-const initialValues = {
-  email: "",
-  password: "",
-};
-
-const validationSchema = {
-  email: {
-    label: "Email",
-    type: "email",
-    rules: {
-      required: true,
-    },
-  },
-  password: {
-    label: "Password",
-    type: "password",
-    rules: {
-      required: true,
-      // min: 8,
-    },
-  },
-};
-
-const FieldWrapper = ({ loginForm, setLoginForm, ...rest }) => (
-  <FormField
-    {...rest}
-    {...loginForm}
-    onChange={(value) => {
-      let a = { ...loginForm.values };
-      a[rest.name] = value;
-      setLoginForm((old) => ({ ...old, values: a }));
-    }}
-  />
-);
 
 export const Login = () => {
-  const navigate = useNavigate();
+  let existingData = JSON.parse(localStorage.getItem("all_users_data"));
+  
+  const validationSchema = yup.object().shape({
+    email: yup
+      .string()
+      .trim()
+      .test({
+        name: "email",
+        skipAbsent: true,
+        test(value, ctx) {
+          if (value.trim() === "") {
+            return ctx.createError({
+              message: `**Email can't be empty..!`,
+            });
+          } else {
+            let flag = 0;
 
-  const [loginForm, setLoginForm] = useState({
-    values: initialValues,
-    errors: {},
-    submit: false,
+            for (let i in existingData) {
+              if (existingData[i].email === value) {
+                flag = 1;
+                break;
+              }
+            }
+
+            if (flag == 0) {
+              return ctx.createError({
+                message: `**Email doesn't exist..!`,
+              });
+            }
+          }
+          return true;
+        },
+      }),
+    password: yup
+      .string()
+      .trim()
+      .test({
+        name: "password",
+        skipAbsent: true,
+        test(value, ctx) {
+          if (value.trim() === "") {
+            return ctx.createError({
+              message: `**Password can't be empty..!`,
+            });
+          } else {
+            let flag = 0;
+
+            for (let i in existingData) {
+              if (
+                existingData[i].email === this.parent.email &&
+                existingData[i].pass === value
+              ) {
+                flag = 1;
+                break;
+              }
+            }
+
+            if (flag == 0) {
+              return ctx.createError({
+                message: `**Password doesn't match..!`,
+              });
+            }
+          }
+          return true;
+        },
+      }),
   });
 
-  useEffect(() => {
-    if (loginForm.submit === true) {
-      validateFormValues();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(validationSchema),
+    mode: "all",
+  });
+
+  const navigate = useNavigate();
+
+  const onSubmit = async (data) => {
+    let randomstr = "";
+    randomstr +=
+      data.email + data.email.split("").reverse().join("") + data.password;
+
+    let flag = 0;
+    let auth_data = {};
+
+    for (let i in existingData) {
+      if (
+        existingData[i].email === data.email &&
+        existingData[i].pass === data.password
+      ) {
+        auth_data = existingData[i];
+        flag = 1;
+        break;
+      }
     }
-  }, [loginForm.values]);
 
-  const validateFormValues = () => {
-    const tempErrors = { ...loginForm.errors };
-    Object.keys(validationSchema).forEach((field) => {
-      const errors = getFieldErrors(validationSchema[field], field);
-      if (errors.length) {
-        tempErrors[field] = errors;
-      } else {
-        // delete field errors if exists
-        if (tempErrors[field]) {
-          delete tempErrors[field];
-        }
-      }
-    });
-    setLoginForm((old) => ({ ...old, errors: tempErrors }));
-    return tempErrors;
-  };
-
-  const fieldPropsCommon = {
-    loginForm,
-    setLoginForm,
-  };
-
-  const getFieldErrors = (fieldInfo, fieldName) => {
-    return (
-      Object.keys(fieldInfo["rules"])
-        .map((ruleType) => {
-          switch (ruleType) {
-            case "isExist":
-              return validateExist(
-                fieldInfo["rules"][ruleType],
-                fieldName,
-                loginForm.values[fieldName],
-                fieldInfo["type"]
-              );
-            default:
-              // required
-              return (
-                !loginForm.values[fieldName].trim() &&
-                `${fieldInfo["label"]} is required`
-              );
-          }
-        })
-        // remove false items from the validation messages
-        .filter((item) => item)
-    );
-  };
-
-  const validateExist = (ruleValue, fieldName, fieldValue, fieldType) => {
-    // switch (fieldType) {
-    //   case "file":
-    //     return (
-    //       fieldValue.size > ruleValue &&
-    //       `${fieldName} size cannot exceed ${ruleValue} KB`
-    //     );
-    //   case "number":
-    //     return (
-    //       fieldValue > ruleValue &&
-    //       `${fieldName} cannot exceed ${ruleValue} INR`
-    //     );
-    //   default:
-    //     // for rest of the fields
-    //     return (
-    //       fieldValue.length > ruleValue &&
-    //       `${fieldName} cannot exceed ${ruleValu e} in length`
-    //     );
-    // }
-  };
-
-  const onSubmit = (e) => {
-    e.preventDefault();
-
-    setLoginForm((old) => ({ ...old, submit: true }));
-
-    const newErrors = validateFormValues();
-    if (Object.keys(newErrors).length === 0) {
-      let existingData = JSON.parse(localStorage.getItem("all_users_data"));
-
-      if (existingData) {
-        let randomstr = "";
-        randomstr +=
-          loginForm.values.email +
-          loginForm.values.email.split("").reverse().join("") +
-          loginForm.values.password;
-
-        let flag = 0;
-        let auth_data = {};
-
-        for (let i in existingData) {
-          if (
-            existingData[i].email === loginForm.values.email &&
-            existingData[i].pass === loginForm.values.password
-          ) {
-            auth_data = existingData[i];
-            flag = 1;
-            break;
-          }
-        }
-
-        if (flag === 1) {
-          auth_data["token"] = randomstr;
-          localStorage.setItem("auth_token", JSON.stringify(auth_data));
-          navigate("/transactions");
-        } else {
-          alert("Email or Password is incorrect..!");
-        }
-      } else {
-        navigate("/register");
-      }
-      // const valueToSave = { ...values };
-      // delete valueToSave['receipt_obj']
-      // save(valueToSave);
-      // navigate("/transactions");
+    if (flag === 1) {
+      auth_data["token"] = randomstr;
+      localStorage.setItem("auth_token", JSON.stringify(auth_data));
+      navigate("/transactions");
+    } else {
+      alert("Email or Password is incorrect..!");
     }
   };
 
   return (
-    <>
-      <div className="container">
-        <div className="loginform">
-          <div className="logincontent">
-            <h1>Login Page</h1>
-
-            <br></br>
-            <br></br>
-            <form onSubmit={(e) => onSubmit(e)}>
-              <div className="inputs">
-                <FieldWrapper
-                  type="text"
-                  label="Email"
-                  name="email"
-                  {...fieldPropsCommon}
-                />
-                <br />
-                <FieldWrapper
-                  type="text"
-                  label="Password"
-                  name="password"
-                  {...fieldPropsCommon}
-                />
-              </div>
-              <br></br>
-
-              <input type="submit" name="submit" value={"Login"} />
-              <br></br>
-              <br></br>
-              <h3>
-                Don't have an account..! <Link to={"/register"}>Sign-up</Link>
-              </h3>
-            </form>
+    <div className="container">
+      <div className="formdiv">
+        <form className="form" onSubmit={handleSubmit(onSubmit)}>
+          <h2>Login form</h2>
+          <br></br>
+          <br></br>
+          <label>Email :-</label>
+          <input type="email" className="forminputs" {...register("email")} />
+          {errors.email && (
+            <p style={{ color: "red" }}>{errors.email?.message}</p>
+          )}
+          <br />
+          <br />
+          <label>Password :-</label>
+          <input
+            type="password"
+            className="forminputs"
+            {...register("password")}
+          />
+          {errors.password && (
+            <p style={{ color: "red" }}>{errors.password?.message}</p>
+          )}
+          <br />
+          <br />
+          <div className="actions">
+            <input type="submit" name="submit" value={"Submit"} />
           </div>
-        </div>
+          <br />
+          <br />
+          Don't have an Account..? <Link to={`/register`}>Sign up</Link>
+        </form>
       </div>
-    </>
+    </div>
   );
 };
