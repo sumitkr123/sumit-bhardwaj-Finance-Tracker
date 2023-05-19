651b53bbc747b9a28b2899681ce717276b068117
Sumit-Bhardwaj <sumit.bhardwaj.eSparkbiz@gmail.com>
2023-05-19 18:39:26 +0530

    updated Transaction table header component and Error Boundary and routing

--- src/routes/routing.js
+++ src/routes/routing.js
@@ -44,7 +44,7 @@ const routeMapper = (item) => {
                     )}
                   </ErrorBoundary>
                 }
-              ></Route>
+              />
             );
         }
       })}
